865dc49b03c08dcc270ef6f77c9e00697b41e160
Dongzhuoran Zhou <dongzhuoran.zhou@outlook.com>
2026-03-19 09:21:50 +0000

    add readme_personal and other shell file for search r1 customized version test

--- examples/search-r1/run_qwen2.5_3B_test_zhipu_wandb.sh
+++ examples/search-r1/run_qwen2.5_3B_test_zhipu_wandb.sh
@@ -18,63 +18,51 @@ export XDG_CACHE_HOME="/workspace/.cache"
 export XDG_DATA_HOME="/workspace/.local/share"
 export XDG_STATE_HOME="/workspace/.local/state"
 
-
 # will prevent ray from buffering stdout/stderr
 export PYTHONBUFFERED=16
 export WANDB_KEY="local-643dfe5b804d35d8ab5aeba347f30f4ee0c6ca6f"
 
-NVLINK_COUNT=$(nvidia-smi topo -m 2>/dev/null | grep -o 'NV[0-9][0-9]*' | wc -l)
-if [ "$NVLINK_COUNT" -gt 0 ]; then
-    HAS_NVLINK=1
-else
-    HAS_NVLINK=0
-fi
-echo "HAS_NVLINK: $HAS_NVLINK (detected $NVLINK_COUNT NVLink references)"
 
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-source "${SCRIPT_DIR}/models/glm4-9B.sh"
+source "${SCRIPT_DIR}/../../scripts/models/qwen2.5-3B.sh"
 
 CKPT_ARGS=(
-   --hf-checkpoint /workspace/.cache/huggingface/hub/GLM-Z1-9B-0414
-   --ref-load /workspace/.cache/huggingface/hub/GLM-Z1-9B-0414_torch_dist
-   # If empty or doesn't contain a valid checkpoint, loads from --ref-load instead, so please comment --load
-   # --load /workspace/.cache/huggingface/hub/GLM-Z1-9B-0414_slime/ # 
-   --save /lc3T/GLM-Z1-9B-0414_slime_original/
+   --hf-checkpoint /workspace/.cache/huggingface/hub/Qwen2.5-3B/
+   --ref-load /workspace/.cache/huggingface/hub/Qwen2.5-3B_torch_dist/
+   # --load /workspace/.cache/huggingface/hub/Qwen2.5-3B_slime/
+   --save /lc3T/Qwen2.5-3B_slime/
    --save-interval 3
 )
 
 ROLLOUT_ARGS=(
-   --prompt-data /workspace/.cache/huggingface/datasets/dapo-math-17k/dapo-math-17k.jsonl
+   --prompt-data /workspace/src/clean_code_for_rl/Search-R1/data/nq_hotpotqa_train/train.parquet
    --input-key prompt
-   --label-key label
+   --label-key reward_model
    --apply-chat-template
    --rollout-shuffle
-
-   --rm-type deepscaler
-
    --num-rollout 5
-   --rollout-batch-size 8
+   --rollout-batch-size 2
    --n-samples-per-prompt 2
-   --rollout-max-response-len 8192
+   --rollout-max-response-len 512
    --rollout-temperature 1
 
-   --global-batch-size 16
-   --balance-data
-)
+   # eval args
+   # --eval-interval 25
+   # --eval-prompt-data nq_test /root/Search-R1/data/nq_hotpotqa_train/test.parquet@[0:3000]
+   # # --eval-prompt-data nq_test /root/nq_search/test.parquet
+   # --eval-input-key prompt
+   # --eval-label-key reward_model
+   # --n-samples-per-eval-prompt 1
 
-EVAL_ARGS=(
-   --eval-interval 3
-   --eval-prompt-data aime /workspace/.cache/huggingface/datasets/aime-2024/aime-2024.jsonl
-   --n-samples-per-eval-prompt 2
-   --eval-max-response-len 16384
-   --eval-top-p 1
+   --global-batch-size 4
+   --balance-data
 )
 
 PERF_ARGS=(
    --tensor-model-parallel-size 2
    --sequence-parallel
    --pipeline-model-parallel-size 1
-   --context-parallel-size 2
+   --context-parallel-size 1
    --expert-model-parallel-size 1
    --expert-tensor-parallel-size 1
 
@@ -84,38 +72,43 @@ PERF_ARGS=(
 
    # --micro-batch-size 1
    --use-dynamic-batch-size
-   --max-tokens-per-gpu 4608
+   --max-tokens-per-gpu 9216
 )
 
 GRPO_ARGS=(
    --advantage-estimator grpo
    --use-kl-loss
-   --kl-loss-coef 0.00
+   --kl-loss-coef 0.001
    --kl-loss-type low_var_kl
    --entropy-coef 0.00
    --eps-clip 0.2
    --eps-clip-high 0.28
+
+   # whether enabling TIS
+   # --use-tis
 )
 
 OPTIMIZER_ARGS=(
    --optimizer adam
    --lr 1e-6
    --lr-decay-style constant
-   --weight-decay 0.1
+   --weight-decay 0.01
    --adam-beta1 0.9
    --adam-beta2 0.98
 )
 
 WANDB_ARGS=(
    --use-wandb
-   --wandb-project slime-zhipu-wandb-debug
-   --wandb-group qwen3-9B-test
+   --wandb-project slime-dev
+   --wandb-group search-r1_qwen2.5-3B-test
    --wandb-key ${WANDB_KEY}
    --wandb-host https://wandb.glm.ai
+
 )
 
 SGLANG_ARGS=(
    --rollout-num-gpus-per-engine 2
+   --sglang-mem-fraction-static 0.7
 )
 
 MISC_ARGS=(
@@ -129,24 +122,25 @@ MISC_ARGS=(
    --attention-backend flash
 )
 
+CUSTOM_ARGS=(
+   --custom-generate-function-path generate_with_search.generate
+   --custom-rm-path generate_with_search.reward_func
+
+   # TIS-related args, recommended to enable when using TIS
+   # --custom-config-path examples/train_infer_mismatch_helper/mis.yaml
+   # --custom-tis-function-path examples.train_infer_mismatch_helper.mis.compute_mis_weights_with_cp
+)
+
 # launch the master node of ray in container
 export MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
+ray start --head --node-ip-address ${MASTER_ADDR} --num-gpus 8 --disable-usage-stats
 
-# export no_proxy="localhost,127.0.0.1,${LOCAL_IP},${MASTER_ADDR},10.0.0.0/8,172.16.0.0/12,192.168.0.0/16"
-# export NO_PROXY="${no_proxy}"
-
-# export no_proxy="localhost,127.0.0.1,${LOCAL_IP},${MASTER_ADDR},10.,172.16.,172.17.,172.18.,172.19.,172.2,192.168."
-export no_proxy="localhost, 127.0.0.1,${LOCAL_IP},${MASTER_ADDR},platform.glm.ai,10.*,192.168.*,172.16.*,172.17.*,172.18.*,172.19.*,172.20.*,172.21.*,172.22.*"
-
-export NO_PROXY="${no_proxy}"
-ray start --head --node-ip-address ${MASTER_ADDR} --num-gpus 8 --disable-usage-stats --dashboard-host=0.0.0.0 --dashboard-port=8265
-
-# Build the runtime environment JSON with proper variable substitution
 RUNTIME_ENV_JSON="{
   \"env_vars\": {
-    \"PYTHONPATH\": \"/workspace/Megatron-LM/\",
+    \"PYTHONPATH\": \"/workspace/Megatron-LM/:${SCRIPT_DIR}\",
     \"CUDA_DEVICE_MAX_CONNECTIONS\": \"1\",
-    \"NCCL_NVLS_ENABLE\": \"${HAS_NVLINK}\"
+    \"WANDB_API_KEY\": \"${WANDB_KEY}\",
+   \"WANDB_BASE_URL\": \"https://wandb.glm.ai\"
   }
 }"
 
@@ -156,6 +150,7 @@ ray job submit --address="http://127.0.0.1:8265" \
    --actor-num-nodes 1 \
    --actor-num-gpus-per-node 4 \
    --rollout-num-gpus 4 \
+   --colocate \
    ${MODEL_ARGS[@]} \
    ${CKPT_ARGS[@]} \
    ${ROLLOUT_ARGS[@]} \
@@ -163,6 +158,6 @@ ray job submit --address="http://127.0.0.1:8265" \
    ${GRPO_ARGS[@]} \
    ${WANDB_ARGS[@]} \
    ${PERF_ARGS[@]} \
-   ${EVAL_ARGS[@]} \
    ${SGLANG_ARGS[@]} \
-   ${MISC_ARGS[@]}
+   ${MISC_ARGS[@]} \
+   ${CUSTOM_ARGS[@]}
